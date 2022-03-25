305e7dfc4a1a3a62da01740b6c073740e7d65c8d
danush <danushk97@gmail.com>
2022-03-25 22:19:43 +0530

    renamed test_app to test_manage

--- tests/test_manage.py
+++ tests/test_manage.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from user_accounts.manage import create_app
 
+
 def test_create_app_should_return_instance_of_flask_app():
     app = create_app()
     assert isinstance(app, Flask)

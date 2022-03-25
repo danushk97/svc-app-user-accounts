7ac60219ce6d775af697371c4395e042e1cac978
danush <danushk97@gmail.com>
2022-03-25 22:15:34 +0530

    updated abstract unit of work

--- src/user_accounts/infrastructure/_unit_of_work.py
+++ src/user_accounts/infrastructure/_unit_of_work.py
@@ -2,15 +2,19 @@
 Holds the abstract class for unit of work.
 """
 
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class AbstractUnitOfWork(ABC):
+
+    @abstractmethod
     def commit(self):
         raise NotImplementedError
 
+    @abstractmethod
     def rollback(self):
         raise NotImplementedError
 
+    @abstractmethod
     def end_session(self):
         raise NotImplementedError

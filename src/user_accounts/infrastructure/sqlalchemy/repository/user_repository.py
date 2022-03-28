8f57fb28caafecc6b310031a07ea213d2590f029
danush <danushk97@gmail.com>
2022-03-29 00:32:58 +0530

    linting fix

--- src/user_accounts/infrastructure/sqlalchemy/repository/user_repository.py
+++ src/user_accounts/infrastructure/sqlalchemy/repository/user_repository.py
@@ -3,9 +3,9 @@ This modle holds the repository class for User.
 """
 
 from sqlalchemy.exc import SQLAlchemyError
-from user_accounts.domain.entity.user import User
 from apputils.error_handler import ErrorHandler
 
+from user_accounts.domain.entity.user import User
 from user_accounts.infrastructure._repository.user_repository import AbstractUserRepository
 from user_accounts.common.exception import RepositoryException
 from user_accounts.infrastructure.sqlalchemy.models.user import UserModel
@@ -47,9 +47,8 @@ class UserRepository(AbstractUserRepository):
             RepositoryException: On SQLAlachemyError
         """
         result = self._session\
-                     .query(UserModel)\
-                     .filter(UserModel.attr[attr_field].astext \
-                             == str(attr_field_value))\
-                     .all()
+            .query(UserModel)\
+            .filter(UserModel.attr[attr_field].astext == str(attr_field_value))\
+            .all()
 
         return result

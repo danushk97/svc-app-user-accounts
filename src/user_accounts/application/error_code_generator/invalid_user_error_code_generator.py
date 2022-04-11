4c677723d0a8e4bdff19fa3d72edbb46734e0657
danush <danushk97@gmail.com>
2022-04-11 11:36:02 +0530

    updated docstring

--- src/user_accounts/application/error_code_generator/invalid_user_error_code_generator.py
+++ src/user_accounts/application/error_code_generator/invalid_user_error_code_generator.py
@@ -9,11 +9,19 @@ from user_accounts.common.error_message import AppErrorMessage
 class InvalidUserErrorCodeGenerator:
     """
     Validates the data which comes along with the request.
-
-    Attributes:
-        (dict): Holds the field and message.
     """
     def generate_duplicate_user_error_code(self, duplicate_field: str):
+        """
+        Returns a dict containing error info.
+
+        Args:
+            duplicate_field (str): A field name.
+
+        Returns:
+            (dict):
+                field (str): Duplicate field name.
+                message (str): An error message.
+        """
         duplicate_field = str(duplicate_field).lower()
 
         if duplicate_field == Constants.EMAIL:
